9e7d55b3168be47c994d4abc8e265f6a4402c701
David Sally <davidcsally@gmail.com>
2017-10-05 11:35:15 -0700

    no longer used

--- js/drawTree.js
+++ js/drawTree.js
@@ -1,262 +1,324 @@
-var treeData =
-  {
-    name: 'Provider',
-    state: null,
-    children: [
-      {
-        name: 'BrowserRouter',
-        state: null,
-        children: [
-          {
-            name: 'Router',
-            state: null,
-            children: [
-              {
-                name: 'Switch',
-                state: null,
-                children: [
-                  {
-                    name: 'Route',
-                    state: null,
-                    children: [
-                      {
-                        name: 'App',
-                        state: null,
-                        children: [
-                          {
-                            name: 'div',
-                            state: null,
-                            children: [
-                              {
-                                name: 'Login',
-                                state: null,
-                                children: [
-                                  {
-                                    name: 'div',
-                                    state: null,
-                                    children: [
-                                      {
-                                        name: 'form',
-                                        state: null,
-                                        children: [
-                                          {
-                                            name: 'input',
-                                            state: null
-                                          },
-                                          {
-                                            name: 'input',
-                                            state: null,
-                                          },
-                                          {
-                                            name: 'button',
-                                            state: null,
-                                          }
-                                        ]
-                                      }
-                                    ]
-                                  }
-                                ]
-                              }
-                            ]
-                          }
-                        ]
-                      }
-                    ]
-                  }
-                ]
-              }
-            ]
-          },
-        ]
-      },
-    ]
-  };
+// const testData =
+//   {
+//     name: 'Provider',
+//     state: null,
+//     id: 10,
+//     children: [
+//       {
+//         name: 'BrowserRouter',
+//         state: null,
+//         id: 20,
+//         children: [
+//           {
+//             name: 'Router',
+//             state: null,
+//             id: 30,
+//             children: [
+//               {
+//                 name: 'Switch',
+//                 state: null,
+//                 id: 40,
+//                 children: [
+//                   {
+//                     name: 'Route',
+//                     state: null,
+//                     id: 50,
+//                     children: [
+//                       {
+//                         name: 'App',
+//                         state: null,
+//                         id: 60,
+//                         children: [
+//                           {
+//                             name: 'div',
+//                             state: null,
+//                             id: 70,
+//                             children: [
+//                               {
+//                                 name: 'Login',
+//                                 state: {
+//                                   username: 'hello',
+//                                   password: '*********'
+//                                 },
+//                                 id: 80,
+//                                 children: [
+//                                   {
+//                                     name: 'div',
+//                                     state: null,
+//                                     id: 90,
+//                                     children: [
+//                                       {
+//                                         name: 'form',
+//                                         state: null,
+//                                         id: 100,
+//                                         children: [
+//                                           {
+//                                             name: 'input',
+//                                             id: 110,
+//                                             state: null
+//                                           },
+//                                           {
+//                                             name: 'input',
+//                                             id: 120,
+//                                             state: null,
+//                                           },
+//                                           {
+//                                             name: 'button',
+//                                             id: 130,
+//                                             state: null,
+//                                           }
+//                                         ]
+//                                       }
+//                                     ]
+//                                   }
+//                                 ]
+//                               }
+//                             ]
+//                           }
+//                         ]
+//                       }
+//                     ]
+//                   }
+//                 ]
+//               }
+//             ]
+//           },
+//         ]
+//       },
+//     ]
+//   };
 
-document.addEventListener('DOMContentLoaded', () => {
-  function update(source) {
-    // Creates a curved (diagonal) path from parent to the child nodes
-    const diagonal = (s, d) => {
-      const path = 'M' + s.x + ',' + s.y
-        + 'C' + s.x + ',' + (s.y + d.y) / 2
-        + ' ' + d.x + ',' + (s.y + d.y) / 2
-        + ' ' + d.x + ',' + d.y
-      return path
-    }
+// document.addEventListener('DOMContentLoaded', () => {
+//   /**
+//    * Abstracting the drawing function to one command.
+//    * This func conditionally renders based on the router and redux checkboxes
+//    */
+//   const draw = () => {
+//     const hideDOM = document.querySelector('#dom-btn').checked
+//     const hideRedux = document.querySelector('#redux-btn').checked
+//     const hideRouter = document.querySelector('#router-btn').checked
 
-    // Toggle children on click.
-    const click = d => {
-      if (d.children) {
-        d._children = d.children;
-        d.children = null;
-      } else {
-        d.children = d._children;
-        d._children = null;
-      }
-      // d3.selectAll("text").attr("class", "text");
-      update(d);
-    }
+//     let datas = testData
+//     if (hideRedux) datas = filterRedux(datas)
+//     if (hideDOM) datas = filterDOM(datas)
+//     if (hideRouter) datas = filterRouter(datas)
+//     // update(datas)
 
-    console.log('Updating Tree with current source...', source)
+//     console.log('datas', datas)
+//     drawChart(datas)
+//   }
 
-    treemap = d3.tree()
-      .nodeSize([hSlider * 5, hSlider * 5])
+//   /**
+//    * DRAW THE CHART
+//    * @param {*} treeData 
+//    */
+//   function drawChart(treeData) {
+//     // declares a tree layout and assigns the size
+//     treemap = d3.tree()
+//       .size([height - 500, width - 500])
 
-    // Assigns the x and y position for the nodes
-    var treeData = treemap(root);
+//     // Assigns parent, children, height, depth
+//     root = d3.hierarchy(treeData, d => d.children);
+//     root.x0 = height - 500 / 2;
+//     root.y0 = 0;
+//     update(root);
+//   }
 
-    // Compute the new tree layout.
-    var nodes = treeData.descendants()
-    var links = treeData.descendants().slice(1)
+//   /**
+//    *  UPDATE THE CHART
+//    * @param {*} source 
+//    */
+//   function update(source) {
+//     // Creates a curved (diagonal) path from parent to the child nodes
+//     const diagonal = (s, d) => {
+//       const path = 'M' + s.x + ',' + s.y
+//         + 'C' + s.x + ',' + (s.y + d.y) / 2
+//         + ' ' + d.x + ',' + (s.y + d.y) / 2
+//         + ' ' + d.x + ',' + d.y
+//       return path
+//     }
 
-    // Normalize for fixed-depth.
-    nodes.forEach(d => { d.y = d.depth * vSlider * 10 }); // magic number is distance between each node
+//     // Toggle children on click.
+//     const click = d => {
+//       if (d.children) {
+//         d._children = d.children;
+//         d.children = null;
+//       } else {
+//         d.children = d._children;
+//         d._children = null;
+//       }
+//       // d3.selectAll("text").attr("class", "text");
+//       update(d);
+//     }
 
-    // ****************** Nodes section ***************************
+//     console.log('Updating Tree with current source...', source)
 
-    // Update the nodes...
-    var node = svg.selectAll('g.node')
-      .data(nodes, function(d) {return d.id || (d.id = ++i); });
+//     treemap = d3.tree()
+//       .nodeSize([hSlider * 5, hSlider * 5])
 
-    // Remove any exiting nodes
-    var nodeExit = node.exit().transition()
-      .duration(duration)
-      .attr('transform', d => 'translate(' + source.x + ',' + source.y + ')')
-      .remove();
+//     // Assigns the x and y position for the nodes
+//     var treeData = treemap(root);
 
-    // Enter any new modes at the parent's previous position.
-    var nodeEnter = node.enter().append('g')
-      .attr('class', 'node')
-      .attr("transform", function (d) {
-        return "translate(" + source.x0 + "," + source.y0 + ")";
-      })
-      .on('click', click);
+//     // Compute the new tree layout.
+//     var nodes = treeData.descendants()
+//     var links = treeData.descendants().slice(1)
 
-    // Add Circle for the nodes
-    nodeEnter.append('circle')
-      .attr('class', 'node')
-      .attr('r', 5)
-      .style('fill', d => d._children ? 'lightsteelblue' : '#fff')
-      .style('pointer-events', 'visible')
+//     // Normalize for fixed-depth.
+//     nodes.forEach(d => { d.y = d.depth * vSlider * 10 }); // magic number is distance between each node
 
-    // Add labels for the nodes
-    nodeEnter.append('text')
-      .attr('dy', '.35em')
-      .attr('y', d => d.children || d._children ? -24 : 24)
-      .attr('text-anchor', 'middle')
-      .text(d => d.data.name)
+//     // ****************** Nodes section ***************************
 
-    // UPDATE
-    var nodeUpdate = nodeEnter.merge(node);
+//     // Update the nodes...
+//     var node = svg.selectAll('g.node')
+//       .data(nodes, d => {
+//         // console.log('node D ', d)
+//         d.data.id
+//       });
+//     // .data(nodes, function (d) { return d.id || (d.id = ++i); });
 
-    // Transition to the proper position for the node
-    nodeUpdate.transition()
-      .duration(duration)
-      .attr('transform', d => 'translate(' + d.x + ',' + d.y + ')');
+//     // Remove any exiting nodes
+//     var nodeExit = node.exit().transition()
+//       .duration(duration)
+//       .attr('transform', d => 'translate(' + source.x + ',' + source.y + ')')
+//       .remove();
 
-    // Update the node attributes and style
-    nodeUpdate.select('circle.node')
-      .attr('r', 10)
-      .style('fill', d => d._children ? 'lightsteelblue' : '#fff')
-      .attr('cursor', 'pointer');    // On exit reduce the node circles size to 0
+//     // Enter any new modes at the parent's previous position.
+//     var nodeEnter = node.enter().append('g')
+//       .attr('class', 'node')
+//       .attr("transform", function (d) {
+//         return "translate(" + source.x0 + "," + source.y0 + ")";
+//       })
+//       .on('click', click);
 
-    nodeExit.select('circle')
-      .attr('r', 1e-6);
-      
+//     // Add Circle for the nodes
+//     nodeEnter.append('circle')
+//       .attr('class', 'node')
+//       .attr('r', 5)
+//       .style('fill', d => d._children ? 'lightsteelblue' : '#fff')
+//       .style('pointer-events', 'visible')
 
+//     // Add labels for the nodes
+//     nodeEnter.append('text')
+//       .attr('dy', '.35em')
+//       .attr('y', d => d.children || d._children ? -24 : 24)
+//       .attr('text-anchor', 'middle')
+//       .text(d => d.data.name)
 
-    // On exit reduce the opacity of text labels
-    nodeExit.select('text')
-      .style('fill-opacity', 1e-6);
+//     // UPDATE
+//     var nodeUpdate = nodeEnter.merge(node);
 
-    // ****************** links section ***************************
+//     // Transition to the proper position for the node
+//     nodeUpdate.transition()
+//       .duration(duration)
+//       .attr('transform', d => 'translate(' + d.x + ',' + d.y + ')');
 
-    // Update the links...
-    var link = svg.selectAll('path.link')
-      .data(links, d => d.id);
+//     // Update the node attributes and style
+//     nodeUpdate.select('circle.node')
+//       .attr('r', 10)
+//       .style('fill', d => d._children ? 'lightsteelblue' : '#fff')
+//       .attr('cursor', 'pointer');    // On exit reduce the node circles size to 0
 
-    // Enter any new links at the parent's previous position.
-    var linkEnter = link.enter().insert('path', "g")
-      .attr("class", "link")
-      .attr('d', function (d) {
-        var o = { x: source.x0, y: source.y0 }
-        return diagonal(o, o)
-      });
+//     nodeExit.select('circle')
+//       .attr('r', 1e-6);
 
-    // UPDATE
-    var linkUpdate = linkEnter.merge(link);
+//     // On exit reduce the opacity of text labels
+//     nodeExit.select('text')
+//       .style('fill-opacity', 1e-6);
 
-    // Transition back to the parent element position
-    linkUpdate.transition()
-      .duration(100)
-      .attr('d', d => diagonal(d, d.parent));
+//     // ****************** links section ***************************
 
-    // Remove any exiting links
-    var linkExit = link.exit().transition()
-      .duration(200)
-      .attr('d', d => {
-        var o = { x: source.x, y: source.y }
-        return diagonal(o, o)
-      })
-      .remove();
+//     // Update the links...
+//     var link = svg.selectAll('path.link')
+//       .data(links, d => d.data.id);
+//     // var link = svg.selectAll('path.link')
+//     //   .data(links, d => d.id);
 
-    // Store the old positions for transition.
-    nodes.forEach(function (d) {
-      d.x0 = d.x;
-      d.y0 = d.y;
-    });
-  }
+//     // Enter any new links at the parent's previous position.
+//     var linkEnter = link.enter().insert('path', "g")
+//       .attr("class", "link")
+//       .attr('d', function (d) {
+//         var o = { x: source.x0, y: source.y0 }
+//         return diagonal(o, o)
+//       });
 
-  function drawChart(treeData) {
-    // declares a tree layout and assigns the size
-    treemap = d3.tree()
-      .size([height - 500, width - 500])
+//     // UPDATE
+//     var linkUpdate = linkEnter.merge(link);
 
-    // Assigns parent, children, height, depth
-    root = d3.hierarchy(treeData, d => d.children);
-    root.x0 = height - 500 / 2;
-    root.y0 = 0;
-    update(root);
-  }
+//     // Transition back to the parent element position
+//     linkUpdate.transition()
+//       .duration(100)
+//       .attr('d', d => diagonal(d, d.parent));
 
-  // **************
-  // **** MAIN ****
-  //***************
-  var i = 0
-  var duration = 500
-  var root
-  var treemap
+//     // Remove any exiting links
+//     var linkExit = link.exit().transition()
+//       .duration(200)
+//       .attr('d', d => {
+//         var o = { x: source.x, y: source.y }
+//         return diagonal(o, o)
+//       })
+//       .remove();
 
-  var hSlider = 10
-  var vSlider = 10
+//     // Store the old positions for transition.
+//     nodes.forEach(function (d) {
+//       d.x0 = d.x;
+//       d.y0 = d.y;
+//     });
+//   }
 
-  var margin = { top: 50, right: 50, bottom: 50, left: 50 }
-  var width = 1000 - margin.right - margin.left
-  var height = 960 - margin.top - margin.bottom
+//   // **************
+//   // **** MAIN ****
+//   //***************
+//   var i = 0
+//   var duration = 500
+//   var root
+//   var treemap
 
-  d3.select("#vSlider").on("input", () => {
-    let val = document.querySelector('#vSlider').value
-    vSlider = val
-    update(root)
-  });
+//   var hSlider = 10
+//   var vSlider = 10
 
-  d3.select("#hSlider").on("input", () => {
-    let val = document.querySelector('#hSlider').value
-    hSlider = val
-    update(root)
-  });
+//   var margin = { top: 50, right: 50, bottom: 50, left: 50 }
+//   var width = 1000 - margin.right - margin.left
+//   var height = 960 - margin.top - margin.bottom
 
-  var svg = d3.select(".tree").append("svg")
-    .attr("width", '100%')
-    .attr("height", '500')
-    .attr('viewBox', '0 0 ' + Math.min(width, 500) + ' ' + Math.min(width, 500))
-    .attr('preserveAspectRatio', 'xMinYMin')
-    .call(d3.zoom()
-      .scaleExtent([0.05, 2])
-      .on('zoom', () => {
-        svg.attr('transform', d3.event.transform)
-      }))
-    .append("g")
-    .attr("transform", "translate(" + Math.min(width, height) / 2 + "," + Math.min(width, height) / 2 + ")");
+//   document.querySelector('#router-btn').addEventListener('click', draw)
+//   document.querySelector('#redux-btn').addEventListener('click', draw)
+//   document.querySelector('#dom-btn').addEventListener('click', draw)
 
-  drawChart(treeData)
-})
+//   d3.select("#vSlider").on("input", () => {
+//     let val = document.querySelector('#vSlider').value
+//     vSlider = val
+//     update(root)
+//   });
+
+//   d3.select("#hSlider").on("input", () => {
+//     let val = document.querySelector('#hSlider').value
+//     hSlider = val
+//     update(root)
+//   });
+
+//   var zoom = d3.zoom()
+//     .scaleExtent([0.05, 2])
+//     .on("zoom", zoomed);
+
+//   var svg = d3.select(".tree").append("svg")
+//     .attr("width", '100%')
+//     .attr("height", '500')
+//     .attr('viewBox', '0 0 ' + Math.min(width, 500) + ' ' + Math.min(width, 500))
+//     .attr('preserveAspectRatio', 'xMinYMin')
+//     .call(zoom)
+//     .append("g")
+//     .attr("transform", "translate(" + Math.min(width, height) / 2 + "," + Math.min(width, height) / 2 + ")");
+
+//   var transform = d3.zoomIdentity
+//     .translate(width / 2, height / 4)
+//     .scale(1)
+
+//   d3.select('svg').transition().duration(750).call(zoom.transform, d3.zoomIdentity.translate(width / 2, height / 4).scale(1))
+
+//   function zoomed() {
+//     svg.attr('transform', d3.event.transform)
+//     // slider.property('value', d3.event.scale)
+//   }
+
+//   draw(testData)
+// })

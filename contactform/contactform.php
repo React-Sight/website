bd3e868738811143ab7395d21378ac6eff14793a
David Sally <davidcsally@gmail.com>
2017-10-14 16:21:48 -0700

    enabled contact form

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -7,10 +7,10 @@
 /***************** Configuration *****************/
 
 // Enter your email, where you want to receive the messages.
-$contact_email_to = "reactsight@gmail.com";
+$contact_email_to = "hello@reactsight.com";
 
 // Subject prefix
-$contact_subject_prefix = "Contat Form Message: ";
+$contact_subject_prefix = "Contact Form Message: ";
 
 // Name too short error text
 $contact_error_name = "Name is too short or empty!";
